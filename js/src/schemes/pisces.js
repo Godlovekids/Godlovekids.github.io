11c33c209936526d2ec9f7e146dec2631f9a7621
Godlovekids <594719456@qq.com>
2018-01-07 22:38:40 +0800

    Site updated: 2018-01-07 22:38:35

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,3 @@
-// build time:Tue Jan 02 2018 21:06:46 GMT+0800 (中国标准时间)
+// build time:Sun Jan 07 2018 22:38:31 GMT+0800 (中国标准时间)
 $(document).ready(function(){var n=$(".header-inner");var r=$("#sidebar");var e=function(){return n.height()+CONFIG.sidebar.offset};var i=function(n){return r.css({"margin-top":n})};var a=window.matchMedia("(min-width: 991px)");i(e()).show();a.addListener(function(n){if(n.matches){i(e())}})});
 //rebuild by neat 
